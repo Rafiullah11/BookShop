c5c2b02d3c2932846604cfd6548d74c3fef257e0
Rafiullah Afridi <Rafiullahafridi11@hotmail.com>
2022-05-12 20:36:16 +0500

    Add Category controller and View

---
+++ BookShop/WebBookShop/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBookShop.Models;
+
+namespace WebBookShop.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BookShop/WebBookShop/Program.cs
+++ BookShop/WebBookShop/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebBookShop.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(
+                            builder.Configuration.GetConnectionString("conn")));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
